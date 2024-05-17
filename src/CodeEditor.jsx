9afd7f8575e42f3c7e282556fd08b224dd9aa595
Mohammad Taha Areeb <158285453+mohammad-taha-areeb@users.noreply.github.com>
2024-05-18 00:50:55 +0530

    Update CodeEditor.jsx

--- src/CodeEditor.jsx
+++ src/CodeEditor.jsx
@@ -150,7 +150,7 @@ const HtmlRunner = () => {
             {/* <OffcanvasExample /> */}
 
 
-            <h1 style={'text-align':'center','color':'gold','font-size':'15vh'}>Style Master</h1>
+            <h1 style={{'text-align':'center','color':'gold','font-size':'15vh'}}>Style Master</h1>
             <div className="main-container">
                 <div className="container">
                     <h3>Input</h3>
